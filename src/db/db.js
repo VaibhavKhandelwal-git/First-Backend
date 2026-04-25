c457c94e86094545c635e6c2a828c63803bb1c9d
Vaibhav-Khandelwal007 <vaibhavkhandewal23cs@rnsit.ac.in>
2026-04-26 01:46:19 +0530

    setup backend with MongoDB connection

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import {DB_NAME}from "../costants.js"
+
+const connectDB=async()=>{
+try{
+    const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`MongoDB connected!! host: ${connectionInstance.connection.host}`);
+}catch (error) {
+    console.log(error)
+    process.exit(1)
+}
+}
+
+export default connectDB;
