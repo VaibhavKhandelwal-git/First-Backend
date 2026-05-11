90a0dfae9feac28824fb5cf2eaa035440361154b
Vaibhav-Khandelwal007 <vaibhavkhandewal23cs@rnsit.ac.in>
2026-05-11 15:32:35 +0530

    setup express app and server

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import {DB_NAME}from "../costants.js"
 
+
 const connectDB=async()=>{
 try{
     const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import connectDB from "./db/db.js";
 import dotenv from "dotenv";
+import app from "./app.js"
 
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch(
+    (err)=>{
+        console.error("Error occurred while starting the server:", err);
+    }
+)
