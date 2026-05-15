088676f43a770c4b1257e1ed08f3564e9608f777
Vaibhav-Khandelwal007 <vaibhavkhandewal23cs@rnsit.ac.in>
2026-05-16 01:17:26 +0530

    multer and cloudinary addition

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,24 +1,25 @@
 class apiError extends Error {
     constructor(
-        statuscode,
-        message="Something Went Wrong",
-        errors=[],
-        stack=""
+        statusCode,
+        message = "Something Went Wrong",
+        errors = [],
+        stack = ""
     ) {
         super(message);
-        this.statuscode = statuscode;
-        this.data=null;
-        this.errors=errors;
-        this.message=message;
-        this.success=false;
 
-        if(stack){
-            this.stack=stack;
-        }
-        else{
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+        this.name = this.constructor.name;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
             Error.captureStackTrace(this, this.constructor);
         }
-
     }
 }
+
 export default apiError;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadToCloudinary=async (localfilepath)=>{
+    try
+    {
+        if(!localfilepath) throw new Error(400,"File path is required for upload");
+        const response=await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto",
+        })
+        return response;
+    } 
+    catch (error) 
+    {
+        fs.unlinkSync(localfilepath);
+        //remove the locally saved file if operation got failed
+        return null;
+    }
+    
+};  
+export default uploadToCloudinary;
